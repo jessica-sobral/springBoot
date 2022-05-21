5b6312618e2dc8e8d87ee68ad53fb36f8143db62
Jéssica Sobral <jessicasobral5672@gmail.com>
2022-05-21 11:49:51 -0300

    adicionando requisições PUT e DELETE através do RestTemplate

--- src/main/java/academy/devdojo/springboot2/client/SpringClient.java
+++ src/main/java/academy/devdojo/springboot2/client/SpringClient.java
@@ -37,6 +37,21 @@ public class SpringClient {
                 new HttpEntity<>(samuraiChamploo, createJsonHeader()),
                 Anime.class);
         log.info("saved anime {}", samuraiChamplooSaved);
+
+        Anime animeToBeUpdated = samuraiChamplooSaved.getBody();
+        animeToBeUpdated.setName("Samurai Champloo 2");
+        ResponseEntity<Void> samuraiChamplooUpdated = new RestTemplate().exchange("http://localhost:8080/animes/",
+                HttpMethod.PUT,
+                new HttpEntity<>(animeToBeUpdated, createJsonHeader()),
+                Void.class);
+        log.info(samuraiChamplooUpdated);
+
+        ResponseEntity<Void> samuraiChamplooDeleted = new RestTemplate().exchange("http://localhost:8080/animes/{id}",
+                HttpMethod.DELETE,
+                null,
+                Void.class,
+                animeToBeUpdated.getId());
+        log.info(samuraiChamplooDeleted);
     }
 
     private static org.springframework.http.HttpHeaders createJsonHeader() {
